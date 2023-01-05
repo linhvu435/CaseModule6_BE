0e84862cb349b9d3303172a9e78a7fbee31ce959
linhvu435 <vuhonglinh1904@gmail.com>
2023-01-05 09:40:41 +0700

    update

---
+++ src/main/java/com/example/casemd6be/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.casemd6be.service;
+
+import com.example.casemd6be.model.Product;
+import com.example.casemd6be.repository.IProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    IProductRepo iProductRepo;
+
+    public void save(Product product) {
+        iProductRepo.save(product);
+    }
+
+    public Iterable<Product> getAll() {
+        return iProductRepo.findAll();
+    }
+
+    public void delete(long id) {
+        iProductRepo.deleteById(id);
+    }
+
+    public Product finByID(long id) {
+        return iProductRepo.findById(id).get();
+    }
+}
