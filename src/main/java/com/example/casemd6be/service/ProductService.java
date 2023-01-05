9df1f70b653da67bfc0a03f2ff8d810323f01399
linhvu435 <vuhonglinh1904@gmail.com>
2023-01-05 10:26:13 +0700

    update1

--- src/main/java/com/example/casemd6be/service/ProductService.java
+++ src/main/java/com/example/casemd6be/service/ProductService.java
@@ -1,7 +1,7 @@
 package com.example.casemd6be.service;
 
 import com.example.casemd6be.model.Product;
-import com.example.casemd6be.repository.IProductRepo;
+import com.example.casemd6be.repository.manh.IProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
