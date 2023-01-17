48147e46f9a97c3e02e734b1bac819933f3b622d
linhvu435 <vuhonglinh1904@gmail.com>
2023-01-17 14:58:42 +0700

    done

--- src/main/java/com/example/casemd6be/repository/linh/ICommentRepo.java
+++ src/main/java/com/example/casemd6be/repository/linh/ICommentRepo.java
@@ -1,7 +1,12 @@
 package com.example.casemd6be.repository.linh;
 
 import com.example.casemd6be.model.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface ICommentRepo extends PagingAndSortingRepository<Comment,Long> {
+    @Query(nativeQuery = true,value = "select*from comment where product_id = :id ")
+    Iterable<Comment> findAllCommentByIdProduct(long id);
 }
